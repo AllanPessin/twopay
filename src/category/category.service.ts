d0a1d58a0bb46774a3a7313858559fe9187ddb84
AllanPessin <allan.pessin@outlook.com>
2025-04-06 10:12:27 -0300

    feat: cadastro de category com relacionamento entre expense

---
+++ src/category/category.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+
+@Injectable()
+export class CategoryService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createCategoryDto: CreateCategoryDto) {
+    return this.prisma.category.create({
+      data: createCategoryDto,
+    });
+  }
+
+  findAll() {
+    return this.prisma.category.findMany();
+    // findMany({
+    //   include: {
+    //     expenses: true,
+    //   },
+    // });
+  }
+
+  //   findOne(id: number) {
+  //     return `This action returns a #${id} category`;
+  //   }
+
+  //   update(id: number, updateCategoryDto: UpdateCategoryDto) {
+  //     return `This action updates a #${id} category`;
+  //   }
+
+  //   remove(id: number) {
+  //     return `This action removes a #${id} category`;
+  //   }
+}
