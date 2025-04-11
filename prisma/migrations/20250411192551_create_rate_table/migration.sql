ce2c734d2e8e5478de174fd7f9656f5cc5f7a443
AllanPessin <allan.pessin@outlook.com>
2025-04-11 18:01:05 -0300

    feat: migration de taxa criado

---
+++ prisma/migrations/20250411192551_create_rate_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `rate` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `value` DOUBLE NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `rate_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `rate` ADD CONSTRAINT `rate_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
